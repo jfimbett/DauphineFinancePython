85dbb332841e4a8c276587ba0d4e45bb243d5503
jfimbett <jfimbett@gmail.com>
2021-09-15 09:30:07 +0200

    Lesson 2 and Lesson 1 Assignment solution

--- Lesson 1/06 - Assignment Solution.py
+++ Lesson 1/06 - Assignment Solution.py
@@ -25,27 +25,49 @@ def compute_primes(N):
     
     # TODO: Create a list of booleans of size N initialized to true
     #       call it bool_array
-    
+    bool_array = [True for i in range(N)]
 
     # TODO: Create a list of numbers from 1 to N, call is number_array
-   
+    number_array = [ n + 1 for n in range(N)]
 
     # TODO: Create an empty vector where you will store all the prime numbers
     #       call it primes
     primes = []
     
     # TODO: 1 is not a prime, set it in the boolean list to False
-    
+    bool_array[0]=False
     
     for n in range(N):
         if bool_array[n] == True:
             primes.append(number_array[n])
             # TODO: loop through all numbers larger than the current one
             #       and set its value in the boolean list to false if its a multiple
-          
+            for j in range(n+1, N):
+                if number_array[j]%number_array[n]==0:
+                    bool_array[j] = False
 
     
     return primes
 
 #%%
 compute_primes(100)
+# %%
+# Extra, how many prime numbers up to n?
+
+from matplotlib import pyplot as plt
+import math
+N= 100
+x = [n+1 for n in range(1, N)]
+pi_x = [len(compute_primes(n+1)) for n in range(1, N)]
+plt.title("Prime counting function") 
+plt.xlabel("x") 
+plt.ylabel("pi(x)") 
+plt.scatter(x,pi_x) # Plots dots
+plt.show()
+# %%
+
+x_logx = [x[i]/math.log(x[i]) for i in range(len(x))]
+plt.scatter(x,pi_x) # Plots dots
+plt.plot(x,x_logx, 'red', zorder = 2) # Plots line
+plt.show()
+# %%
